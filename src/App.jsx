61d8845486b14648b2ae106d532aa7acccbb9d9a
sharitha-developer <sharitha.developer@gmail.com>
2025-08-27 16:51:00 +1000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import CalendarApp from "./components/calendarApp";
+import "./components/calendarApp.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <CalendarApp />
+    </div>
+  );
+};
+
+export default App;
